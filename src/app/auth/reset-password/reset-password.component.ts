a6380ca43abd56079b1f300aee81cfba912ab4e3
devangp_techuz <devangp@techuz.com>
2020-07-16 20:08:29 +0530

    success message shown after success fully reset

--- src/app/auth/reset-password/reset-password.component.ts
+++ src/app/auth/reset-password/reset-password.component.ts
@@ -3,9 +3,9 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 import { CommonService } from 'app/shared/services/common.service';
 import { ValidationService } from "app/shared/services/validator.service";
-import {NgxSpinnerService} from "ngx-spinner";
-import {ActivatedRoute, ActivatedRouteSnapshot, Router} from '@angular/router';
-import {GlobalService} from '../../shared/services/global.service';
+import { NgxSpinnerService } from "ngx-spinner";
+import { ActivatedRoute, ActivatedRouteSnapshot, Router } from '@angular/router';
+import { GlobalService } from '../../shared/services/global.service';
 
 @Component({
   selector: 'app-reset-password',
@@ -15,25 +15,25 @@ import {GlobalService} from '../../shared/services/global.service';
 export class ResetPasswordComponent implements OnInit {
   resetPasswordForm: FormGroup;
   constructor(
-      private router: Router,
-      private route: ActivatedRoute,
-      private toastr: ToastrService,
-      private commonService: CommonService,
-      private formBuilder: FormBuilder,
-      private validationService: ValidationService,
-      private spinner: NgxSpinnerService,
-      public global: GlobalService
+    private router: Router,
+    private route: ActivatedRoute,
+    private toastr: ToastrService,
+    private commonService: CommonService,
+    private formBuilder: FormBuilder,
+    private validationService: ValidationService,
+    private spinner: NgxSpinnerService,
+    public global: GlobalService
   ) { }
 
   ngOnInit(): void {
     this.resetPasswordForm = this.formBuilder.group({
-      password: ['',[Validators.required,this.validationService.passwordValidator]],
+      password: ['', [Validators.required, this.validationService.passwordValidator]],
       confirm_password: ['', Validators.required],
       emailToken: ['']
     },
-    {
-      validator: this.validationService.MatchPassword('password', 'confirm_password')
-    });
+      {
+        validator: this.validationService.MatchPassword('password', 'confirm_password')
+      });
   }
 
   onSubmit() {
@@ -44,16 +44,16 @@ export class ResetPasswordComponent implements OnInit {
     this.resetPasswordForm.value.emailToken = this.route.snapshot.paramMap.get('id');
     delete this.resetPasswordForm.value.confirm_password;
     this.commonService.resetPassword(this.resetPasswordForm.value).subscribe(
-        (result: any) => {
-          if(result.success){
-            this.global.errorToastr(result.message);
-            this.router.navigateByUrl('login');
-          } else {
-            this.global.errorToastr(result.message);
-          }
-          this.spinner.hide();
-          this.resetPasswordForm.reset();
+      (result: any) => {
+        if (result.success) {
+          this.global.successToastr(result.message);
+          this.router.navigateByUrl('login');
+        } else {
+          this.global.errorToastr(result.message);
         }
+        this.spinner.hide();
+        this.resetPasswordForm.reset();
+      }
     );
   }
 
