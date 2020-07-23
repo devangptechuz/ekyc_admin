137657d1f257f3d396560cd6da76a118c22486ff
pragnesh-techuz <pragnesh@techuz.com>
2020-07-23 11:55:42 +0530

    image upload

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   api_url: "https://9cf1gyerwj.execute-api.us-east-2.amazonaws.com/test/api",
+  imag_url: "https://1ykgrssm65.execute-api.us-east-2.amazonaws.com/test/api",
 };
