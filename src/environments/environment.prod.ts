c2d366081803035b783a3eba53614959bf3bf3b2
devangp_techuz <devangp@techuz.com>
2020-07-27 15:06:02 +0530

    remove unrelated images, favicon added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   api_url: "https://9cf1gyerwj.execute-api.us-east-2.amazonaws.com/test/api",
-  userlimitRow: 3,
-  adminlimitRow: 3,
+  userlimitRow: 10,
+  adminlimitRow: 10,
   imag_url: "https://1ykgrssm65.execute-api.us-east-2.amazonaws.com/test/api"
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   api_url: "https://9cf1gyerwj.execute-api.us-east-2.amazonaws.com/test/api",
-  userlimitRow: 3,
+  userlimitRow: 10,
   adminlimitRow: 10,
   imag_url: "https://1ykgrssm65.execute-api.us-east-2.amazonaws.com/test/api"
 };
