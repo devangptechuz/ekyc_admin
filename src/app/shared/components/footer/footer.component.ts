69df2083ad30aa9594e22fc9bae80bdb4d21dfb1
pragnesh-techuz <pragnesh@techuz.com>
2020-07-29 17:22:50 +0530

    bootom to top button set.

--- src/app/shared/components/footer/footer.component.ts
+++ src/app/shared/components/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, HostBinding ,HostListener} from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
     selector: 'app-footer',
@@ -8,35 +8,7 @@ import { Component, HostBinding ,HostListener} from '@angular/core';
 
 export class FooterComponent{
     //Variables
-    showScroll: boolean;
-    showScrollHeight = 700;
-    hideScrollHeight = 30;
     currentDate : Date = new Date();
     constructor() { }
-
-    @HostListener('window:scroll', [])
-    onWindowScroll()
-    {
-        if (( window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) > this.showScrollHeight)
-        {
-            this.showScroll = true;
-        }
-        else if ( this.showScroll && (window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop) < this.hideScrollHeight)
-        {
-            this.showScroll = false;
-        }
-    }
-
-    scrollToTop()
-    {
-        (function smoothscroll()
-        { var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
-            if (currentScroll > 0)
-            {
-                window.requestAnimationFrame(smoothscroll);
-                window.scrollTo(0, currentScroll - (currentScroll / 5));
-            }
-        })();
-    }
 
 }
